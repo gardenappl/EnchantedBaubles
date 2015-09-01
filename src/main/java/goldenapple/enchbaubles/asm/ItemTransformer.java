356f75a498d4411c1634303f799e0a0cbd0489db
goldenapple3 <goldenappleminecraft@gmail.com>
2015-09-01 12:34:22 +0300

    Whoops. Now it shouldn't crash things.

--- src/main/java/goldenapple/enchbaubles/asm/ItemTransformer.java
+++ src/main/java/goldenapple/enchbaubles/asm/ItemTransformer.java
@@ -21,9 +21,7 @@ public class ItemTransformer implements IClassTransformer {
             ClassReader classReader = new ClassReader(basicClass);
             classReader.accept(classNode, ClassReader.EXPAND_FRAMES);
 
-            for(MethodNode method : classNode.methods){
-                info("Method %s%s", method.name, method.desc);
-
+            for(MethodNode method : classNode.methods){ //iterating through all the methods in the class
                 if(method.name.equals("isItemEnchantable") && method.desc.equals("()Z")){
                     info("Found target method %s%s", method.name, method.desc);
                     patchIsItemEnchantable(method, false);
@@ -38,15 +36,14 @@ public class ItemTransformer implements IClassTransformer {
             classNode.accept(writer);
             return writer.toByteArray();
         }else if(name.equals("net.minecraft.item.Item") || name.equals("adb")){
-            info("Found target class " + name + "(transformed: " + transformedName + ")");
+            info("Found target class %s (transformed: %s)", name, transformedName);
+
             //Read the class
             ClassNode classNode = new ClassNode();
             ClassReader classReader = new ClassReader(basicClass);
             classReader.accept(classNode, ClassReader.EXPAND_FRAMES);
 
             for(MethodNode method : classNode.methods){
-                info("Method %s%s", method.name, method.desc);
-
                 //NOTE: the ItemStack-sensitive version of getItemEnchantability is added by Forge, therefore it's name is never obfuscated.
                 if(method.name.equals("getItemEnchantability") && method.desc.equals("(Lnet/minecraft/item/ItemStack;)I")){
                     info("Found target method %s%s", method.name, method.desc);
@@ -68,7 +65,7 @@ public class ItemTransformer implements IClassTransformer {
     private static void patchGetItemEnchantability(MethodNode methodNode, boolean obfuscated){
         boolean injected = false;
 
-        for(AbstractInsnNode node : methodNode.instructions.toArray()) { //iterating through all the instructions in the class
+        for(AbstractInsnNode node : methodNode.instructions.toArray()) { //iterating through all the instructions in the method
      /* In the Item.getItemEnchantability(ItemStack stack) method:
         By default, it's code is "return getItemEnchantability()", which represents the instructions
         INVOKEVIRTUAL <internal method name> and IRETURN. Our goal is to replace INVOKEVIRTUAL with an
@@ -100,12 +97,12 @@ public class ItemTransformer implements IClassTransformer {
 
         for(AbstractInsnNode node : methodNode.instructions.toArray()){
      /* This method calculates some logic to determine if an item is a damageable tool.
-        Our goal is to skip all those calculations and just call our hook
+        Our goal is to just skip all those calculations, call our hook and return the result
         (see ItemHooks.isItemEnchantable(ItemStack stack)*/
             if(node.getOpcode() == Opcodes.INVOKEVIRTUAL && !injected){ //We'll just find the first called method, override it and return. It's lazy but it works.
                 InsnList toInject = new InsnList();
                 toInject.add(new VarInsnNode(Opcodes.ALOAD, 0)); //Load local variable 0 ("this")
-                toInject.add(new MethodInsnNode(Opcodes.INVOKESTATIC, "goldenapple/enchbaubles/asm/ItemHooks", "isEnchantable", obfuscated ? "(Ladd;)I" : "(Lnet/minecraft/item/ItemStack;)I", false)); //Call our hook
+                toInject.add(new MethodInsnNode(Opcodes.INVOKESTATIC, "goldenapple/enchbaubles/asm/ItemHooks", "isEnchantable", obfuscated ? "(Ladd;)Z" : "(Lnet/minecraft/item/ItemStack;)Z", false)); //Call our hook
                 toInject.add(new InsnNode(Opcodes.IRETURN)); //And return the value that we get
                 methodNode.instructions.insertBefore(node, toInject);
                 injected = true;
